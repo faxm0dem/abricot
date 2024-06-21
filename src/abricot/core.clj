34153f6c1aaf5d73e5b9657f9dd2afd3f930b377
Fabien Wernli <github@faxmodem.org>
2024-06-21 15:13:56 +0200

    First working version

--- src/abricot/core.clj
+++ src/abricot/core.clj
@@ -1,34 +1,26 @@
 (ns abricot.core
-  (:require [mirabelle.action :as a]
-            [mirabelle.io :as io]
+  (:import (io.riemann.riemann.client RiemannClient))
+  (:require [mirabelle.io :as io]
             [com.stuartsierra.component :as component]
-            [riemann.client :refer [tcp-client send-events]]))
+            [riemann.client :refer [tcp-client send-event send-events]]))
 
-(defn keep-if-greater-than*
-  [context threshold & children]
-  (fn [event]
-    (when (> (:metric event) threshold)
-      (a/call-rescue event children))))
-
-(defrecord CustomFileOutput [registry path]
-  io/Output
-  (inject! [this events]
-    (doseq [event events]
-      (spit path (str (pr-str event) "\n") :append true))))
-
-(defrecord RiemannForward [registry config
-                           client]
+(defrecord RiemannForward [config
+                           ^RiemannClient client]
   component/Lifecycle
   (start [this]
     (if client
       this
-      (let [c (riemann.client/tcp-client config)]
+      (let [^RiemannClient c (riemann.client/tcp-client config)]
         (assoc this
                :config config
                :client c))))
   (stop [this]
-    (.close client)
+    (when client
+      (.close client))
     (assoc this :client nil))
   io/Output
   (inject! [this events]
-    @(riemann.client/send-event client events)))
+    (do
+    (if (map? events)
+      (deref (riemann.client/send-event client events))
+      (deref (riemann.client/send-events client events))))))
