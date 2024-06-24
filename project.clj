4955b3e4e7fdbada28533e104b7c7bb4e09a0918
Fabien Wernli <github@faxmodem.org>
2024-06-24 12:33:37 +0200

    Remove unnecessary dep

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"][fr.mcorbin/mirabelle "0.13.0"]]
+  :dependencies [[fr.mcorbin/mirabelle "0.13.0"]]
   :plugins [[lein-localrepo "0.5.4"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
